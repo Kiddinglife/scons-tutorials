b414975b19fa6831334711667f89b06df7846d5d
Your Name <jakezhang1989@hotmail.com>
2017-12-05 20:42:56 +1100

    day01

---
+++ day-1/goodbye.c
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : scons-tutorials.cpp
+// Author      : zmd
+// Version     :
+// Copyright   : BSD-3
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+
+int func() {
+	printf("!!!Hello World!!!\n"); // prints !!!Hello World!!!
+	return 0;
+}
